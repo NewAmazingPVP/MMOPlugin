52f326e78b25b5af09b5bb4f93fd1b7a9f0ea16c
NewAmazingPVP <gamingofpat@gmail.com>
2024-09-11 21:37:22 -0400

    mmo plugin release
    mmo plugin release

---
+++ src/main/java/mmo/mmoplugin/MMOPlugin.java
@@ -0,0 +1,17 @@
+package mmo.mmoplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MMOPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
