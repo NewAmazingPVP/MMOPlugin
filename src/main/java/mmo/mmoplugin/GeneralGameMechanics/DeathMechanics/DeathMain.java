8f415a849766fa3ade16e1b82d9e3a056898207a
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-12 21:24:44 -0400

    Update DeathMain.java

--- src/main/java/mmo/mmoplugin/GeneralGameMechanics/DeathMechanics/DeathMain.java
+++ src/main/java/mmo/mmoplugin/GeneralGameMechanics/DeathMechanics/DeathMain.java
@@ -2,16 +2,28 @@ package mmo.mmoplugin.GeneralGameMechanics.DeathMechanics;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Effect;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.boss.BossBar;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
 public class DeathMain implements Listener {
 
+
+
+    private final ArrayList<String> deathSplashMessages = new ArrayList<>();
     @EventHandler
     public void playerDeathEvent(PlayerDeathEvent e){
 
@@ -20,6 +32,11 @@ public class DeathMain implements Listener {
         e.setCancelled(true);
 
         player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20*4, 1));
+
+        for(int i = 0; i<5; i++) {
+            player.playSound(player.getLocation(), Sound.BLOCK_VAULT_OPEN_SHUTTER, 5.0f, 2.0f);
+        }
+        
 
         player.sendTitle(ChatColor.RED + "☠ " + ChatColor.BOLD +  "[YOU DIED]" + ChatColor.RED +  " ☠", ChatColor.YELLOW + "<Splash text>", 0, 80, 20);
 
