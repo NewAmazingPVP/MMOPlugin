131f201113e778c3d7665da316430eccf0a42935
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-13 22:20:04 -0400

    1

--- src/main/java/mmo/mmoplugin/RegisterEventsAndCMD.java
+++ src/main/java/mmo/mmoplugin/RegisterEventsAndCMD.java
@@ -2,7 +2,7 @@ package mmo.mmoplugin;
 
 import mmo.mmoplugin.GeneralGameMechanics.DeathMechanics.DeathMain;
 
-import static mmo.mmoplugin.MMOPlugin.plugin;
+
 import static org.bukkit.Bukkit.getServer;
 
 public class RegisterEventsAndCMD {
