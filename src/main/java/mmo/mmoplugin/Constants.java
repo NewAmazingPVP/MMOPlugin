095d5d454c3df40b89f67b70bcbcb605d8b06d63
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-14 17:18:30 -0400

    1

---
+++ src/main/java/mmo/mmoplugin/Constants.java
@@ -0,0 +1,10 @@
+package mmo.mmoplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class Constants {
+
+    public static Location respawnLoc1 = new Location(Bukkit.getWorld("MMOWorld"), 278, 271.5, 550);
+
+}
