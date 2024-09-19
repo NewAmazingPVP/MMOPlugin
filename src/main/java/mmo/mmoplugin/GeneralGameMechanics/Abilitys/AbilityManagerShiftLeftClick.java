a3549146644eeabcc1b019071bf19db411fc886b
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-18 22:45:36 -0400

    1

--- src/main/java/mmo/mmoplugin/GeneralGameMechanics/Abilitys/AbilityManagerShiftLeftClick.java
+++ src/main/java/mmo/mmoplugin/GeneralGameMechanics/Abilitys/AbilityManagerShiftLeftClick.java
@@ -25,7 +25,7 @@ public class AbilityManagerShiftLeftClick implements Listener {
             if(player.isSneaking()){
 
 
-                backwardsLaunchAbilityTRIGGER(loc,player); 
+                backwardsLaunchAbilityTRIGGER(loc,player);
 
 
             }
