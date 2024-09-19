02a60d794d4ae5526962932db7a06d44d5d508cd
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-18 22:45:36 -0400

    1

--- src/main/java/mmo/mmoplugin/GeneralGameMechanics/Abilitys/Trigger/backwardsLaunchAbility.java
+++ src/main/java/mmo/mmoplugin/GeneralGameMechanics/Abilitys/Trigger/backwardsLaunchAbility.java
@@ -1,13 +1,21 @@
 package mmo.mmoplugin.GeneralGameMechanics.Abilitys.Trigger;
 
 import org.bukkit.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
+
+import static mmo.mmoplugin.MMOPlugin.MMOPlugin;
 
 public class backwardsLaunchAbility {
 
 
+    public static BukkitRunnable backLaunchParticles;
+
     public static void backwardsLaunchAbilityTRIGGER(Location loc, Player player) {
+
 
 
         World world = loc.getWorld();
@@ -23,6 +31,55 @@ public class backwardsLaunchAbility {
             if (velocity.getY() > 10.0) {
                 velocity.setY(10.0);
             }
+
+            player.setVelocity(velocity);
+
+
+            Location p1 = loc;
+            Location p2 = loc;
+            Location p3 = loc;
+            Location p4 = loc;
+
+            backLaunchParticles = new BukkitRunnable() {
+
+                int animationLoop =0;
+
+                @Override
+                public void run() {
+
+                    animationLoop +=1;
+
+                    if(animationLoop > 10){
+                        this.cancel();
+                        return;
+                    }
+
+                    p1.setY(+.2);
+                    p1.setX(+.5);
+                    p1.setZ(+.5);
+
+                    p2.setY(+.2);
+                    p2.setX(-.5);
+                    p2.setZ(+.5);
+
+                    p3.setY(+.2);
+                    p3.setX(+.5);
+                    p3.setZ(-.5);
+
+                    p4.setY(+.2);
+                    p4.setX(-.5);
+                    p4.setZ(-.5);
+
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        player.getWorld().spawnParticle(Particle.DUST, p1, 0, new Particle.DustOptions(Color.AQUA, 2.0f));
+                        player.getWorld().spawnParticle(Particle.DUST, p2, 0, new Particle.DustOptions(Color.AQUA, 2.0f));
+                        player.getWorld().spawnParticle(Particle.DUST, p3, 0, new Particle.DustOptions(Color.AQUA, 2.0f));
+                        player.getWorld().spawnParticle(Particle.DUST, p4, 0, new Particle.DustOptions(Color.AQUA, 2.0f));
+                    }
+
+                }
+            };
+            backLaunchParticles.runTaskTimer(MMOPlugin, 0L, 5L);
 
 
 
