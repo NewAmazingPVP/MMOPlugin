eb14da97cf458a05b113de618021aee3c602d0a3
Comet991 <139285587+Comet991@users.noreply.github.com>
2024-09-21 16:32:02 -0400

    Update ParticleBallAnimation.java

--- src/main/java/mmo/mmoplugin/GeneralGameMechanics/Abilitys/Animations/ParticleBallAnimation.java
+++ src/main/java/mmo/mmoplugin/GeneralGameMechanics/Abilitys/Animations/ParticleBallAnimation.java
@@ -14,39 +14,35 @@ public class ParticleBallAnimation {
 
     public static BukkitRunnable smallBurstAbilityAnimation;
 
-    public static void ballParticleAnimation(Location loc, Player player, Particle particle, int burstTimes){
+    public static void ballParticleAnimation(Location loc, Player player, Particle particle, int burstTimes) {
 
         World world = loc.getWorld();
         smallBurstAbilityAnimation = new BukkitRunnable() {
 
-            double phi =0;
+            double phi = 0;
 
             //int animationLoop =0;
 
             @Override
             public void run() {
 
-                phi += Math.PI/10; //<< 10 circles HIGHLY RECOMENED DO NOT SO MORE
+                phi += Math.PI / 10; //<< 10 circles HIGHLY RECOMENED DO NOT SO MORE
 
-                for(double theta = 0; theta <= 2*Math.PI; theta += Math.PI/40){
+                for (double theta = 0; theta <= 2 * Math.PI; theta += Math.PI / 40) {
                     double r = 1.5;
-                    double x = r*cos(theta)*sin(phi);
-                    double y = r*cos(phi) + 1.5;
-                    double z = r*sin(theta) * sin(phi);
-                    loc.add(x,y,z);
-                    world.spawnParticle(particle,loc,0,0,0,0,1);
-                    loc.subtract(x,y,z);
+                    double x = r * cos(theta) * sin(phi);
+                    double y = r * cos(phi) + 1.5;
+                    double z = r * sin(theta) * sin(phi);
+                    loc.add(x, y, z);
+                    world.spawnParticle(particle, loc, 0, 0, 0, 0, 1);
+                    loc.subtract(x, y, z);
                 }
 
 
-
-
-                if(phi>Math.PI*burstTimes){
+                if (phi > Math.PI * burstTimes) {
                     this.cancel();
                     return;
                 }
-
-
 
 
             }
@@ -54,6 +50,5 @@ public class ParticleBallAnimation {
         smallBurstAbilityAnimation.runTaskTimer(MMOPlugin, 0L, 0L);
 
     }
-    }
-
 }
+
